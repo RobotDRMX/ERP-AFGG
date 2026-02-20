47a9037b748567e20c5624005196cfb844f40e94
RobotDRMX <hailrobot05@gmail.com>
2026-02-20 17:53:47 -0600

    init proyect

---
+++ erp/src/app/app.ts
@@ -0,0 +1,19 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ButtonModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('erp');
+
+  count = 0;
+
+  increment() {
+    this.count++;
+  }
+}
